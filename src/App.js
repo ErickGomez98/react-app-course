11ad0cab2162b9ca8f391fbf7df62d867c7d0d50
Erick Gomez <erick0gomez98@gmail.com>
2018-06-29 12:22:55 -0500

    Radium package added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -63,7 +64,11 @@ class App extends Component {
       font: 'inheirt',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -83,6 +88,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
     const classes = [];
@@ -107,4 +116,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
