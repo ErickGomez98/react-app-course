9fa4bf4a28de741c34a26f23f31099ee513c5d55
Erick Gomez <erick0gomez98@gmail.com>
2018-06-29 13:00:02 -0500

    Sudo selectors and media queries CSS with radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Radium from 'radium';
+
 import './Person.css';
 
 const Person = (props) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  };
   return (
-      <div className='Person'>
+      <div className='Person' style={style}>
         <h1 onClick={props.click}>Hey I'm a {props.name} and i'm {props.age} years old </h1>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}/>
@@ -11,5 +18,4 @@ const Person = (props) => {
   );
 };
 
-export default Person;
-
+export default Radium(Person);
